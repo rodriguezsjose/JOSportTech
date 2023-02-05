02b833540d474a34d28d91304e75f849a55aec1c
Priscilla <u201601228@upc.edu.pe>
2023-02-05 17:17:59 -0500

    0502

---
+++ app/src/main/java/com/grupouno/josporttech/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.grupouno.josporttech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask tarea  = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(SplashActivity.this, ActivityListarReclamo.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 10000);
+    }
+}
