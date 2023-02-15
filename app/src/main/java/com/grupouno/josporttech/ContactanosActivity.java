b1f7f8f57c1ba359669087d7717112e07816c542
rodriguezsjose <rodriguezs.jose@gmail.com>
2023-02-15 00:46:20 -0500

    Commit - Micro (Contactenos)

--- app/src/main/java/com/grupouno/josporttech/ContactanosActivity.java
+++ app/src/main/java/com/grupouno/josporttech/ContactanosActivity.java
@@ -1,18 +1,27 @@
 package com.grupouno.josporttech;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.telephony.SmsManager;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class ContactanosActivity extends AppCompatActivity {
     EditText etMsj, etCel;
 
 
     Button btnW;
+    FloatingActionButton btnMicro;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +34,6 @@ public class ContactanosActivity extends AppCompatActivity {
         etMsj.setText("Necesito mayor información acerca de mi reserva");
 
         btnW = findViewById(R.id.botonenviar);
-
-
         btnW.setOnClickListener(view -> {
 
 
@@ -46,13 +53,24 @@ public class ContactanosActivity extends AppCompatActivity {
                 intent.setData(Uri.parse(uri));
                 startActivity(intent);
 
-
-
-
             }
         });
 
+        btnMicro = findViewById(R.id.btnMicro);
+        btnMicro.setOnClickListener(v -> {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,"es-MX");
+            startActivityForResult(intent,200);
+        });
+    }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 200 && resultCode == RESULT_OK) {
+            ArrayList<String> arrayList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            String voice = arrayList.get(0);
+            etMsj.setText(voice);
+        }
     }
 }
