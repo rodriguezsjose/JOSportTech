5e31a08bde7ca139d954380a07a1a8a0f1ee5cdb
plecca <u201601228@upc.edu.pe>
2023-02-15 16:46:39 -0500

    15/02

--- app/src/main/java/com/grupouno/josporttech/ActivityDetalleProveedor.java
+++ app/src/main/java/com/grupouno/josporttech/ActivityDetalleProveedor.java
@@ -6,8 +6,7 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityDetalleProveedor extends AppCompatActivity {
-    EditText txtNom;
-            //txtDir, txtHor, txtProv, txtDist, txtRef, txtDep, txtServ, txtGal;
+    EditText txtNom, txtDir, txtHor, txtProv, txtDist, txtRef, txtDep, txtServ, txtGal;
 
     Proveedor p;
     DAOProv prov = new DAOProv(this);
@@ -21,11 +20,25 @@ public class ActivityDetalleProveedor extends AppCompatActivity {
     }
     private void agregarDatos() {
     txtNom.setText(getIntent().getStringExtra("p_nom"));
+    txtDir.setText(getIntent().getStringExtra("p_dir"));
+        txtDir.setText(getIntent().getStringExtra("p_hor"));
+        txtDir.setText(getIntent().getStringExtra("p_prov"));
+        txtDir.setText(getIntent().getStringExtra("p_dist"));
+        txtDir.setText(getIntent().getStringExtra("p_ref"));
+        txtDir.setText(getIntent().getStringExtra("p_dep"));
+        txtDir.setText(getIntent().getStringExtra("p_serv"));
 
     }
 
     private void asignaReferencias() {
        txtNom = findViewById(R.id.txtNombreCD);
+       txtDir = findViewById(R.id.txtDirCD);
+       txtHor = findViewById(R.id.txtHorarioCD);
+       txtProv = findViewById(R.id.txtProvCD);
+       txtDist = findViewById(R.id.txtDisCD);
+       txtRef = findViewById(R.id.txtRef);
+       txtDep = findViewById(R.id.txtDeportesCD) ;
+       txtServ = findViewById(R.id.txtServiciosCD);
 
 
 
